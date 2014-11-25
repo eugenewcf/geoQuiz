edbc8cbd6f3d23a60aee5fc835f69cbf7a9f65b3
Eugene Wong <eugenewcf@gmail.com>
2014-11-26 02:23:36 +0800

    init commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by eugene on 26/11/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        this.mQuestion = question;
+        this.mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
